3749938e468bbb57f1ddd6888d479c5f09b9a95c
vishal-95-um <vishalchhokra9@gmail.com>
2026-08-11 23:53:42 +0530

    add MQTT connection, publish, subscribe and callback handling

---
+++ include/MQTTManager.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+
+class MQTTManager{
+public:
+   MQTTManager();
+
+   bool begin(const char* broker, uint16_t port);
+   bool connect(const char* clientId, const char* username, const char* password);
+
+   bool publish(const char* topic, const char* payload);
+   bool subscribe(const char* topic);
+
+   void loop();
+   void disconnect();
+
+   bool isConnected();
+
+   typedef void (*MQTTCallback)(const char* topic, const char* payload);
+   void setCallback(MQTTCallback callback);
+
+private:
+   WiFiClientSecure wifiClient;
+   PubSubClient mqttClient;
+   MQTTCallback callback;
+}
---
+++ src/MQTTManager.cpp
@@ -0,0 +1,70 @@
+#include "MQTTManager.h"
+
+MQTTManager::MQTTManager()
+   :mqttClient(wifiClient), callback(nullptr)
+{
+}
+
+bool MQTTManager::isConnected() {
+    return mqttClient.connected();
+}
+
+bool MQTTManager::begin(const char* broker, uint16_t port) {
+    wifiClient.setInsecure();
+
+    mqttClient.setClient(wifiClient);
+    mqttClient.setServer(broker, port);
+
+    return true;
+}
+
+bool MQTTManager::connect(const char* clientId, const char* username, const char* password) {
+    if(isConnected()) {
+        return true;
+    }
+
+    return mqttClient.connect(clientId, username, password);
+}
+
+bool MQTTManager::publish(const char* topic, const char* payload) {
+    if(!isConnected()) {
+        return false;
+    }
+
+    return mqttClient.publish(topic, payload);
+}
+
+bool MQTTManager::subscribe(const char* topic) {
+    if(!isConnected()) {
+        return false;
+    }
+
+    return mqttClient.subscribe(topic);
+}
+
+void MQTTManager::loop() {
+    mqttClient.loop();
+}
+
+void MQTTManager::disconnect() {
+    mqttClient.disconnect();
+}
+
+void MQTTManager::setCallback(MQTTCallback callback) {
+    this->callback = callback;
+
+    mqttClient.setCallback(
+        [this](char* topic, byte* payload, unsigned int length)
+        {
+            char message[length + 1];
+
+            memcpy(message, payload, length);
+            message[length] = '\0';
+
+            if (this->callback)
+            {
+                this->callback(topic, message);
+            }
+        }
+    );
+}
